ef7c3a0df64457e4ca2b3d3b0936b9d0a63c022e
janvi rajgor <rajgorjanvi9@gmail.com>
2022-06-08 16:20:50 +0530

    release

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -6,7 +6,7 @@
     <div class="col-12 pt-2">
         <a href="{{ route('post.index')}}" class="btn btn-outline-primary btn-sm"> <i class="fas fa-backward "></i> </a>
         <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
-            <h1 class="display-4">Create a New Post</h1>
+            <h1 class="display-4">Edit Post</h1>
             <p>Fill and submit this form to update a post</p>
             <hr>
 
